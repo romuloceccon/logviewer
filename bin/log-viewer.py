9b5c3d7bf67231fccd00b02dbcd45472f036493f
Rômulo A. Ceccon <romuloceccon@gmail.com>
2016-06-18 20:00:59 -0300

    Allow running application without some drivers

--- bin/log-viewer.py
+++ bin/log-viewer.py
@@ -4,13 +4,25 @@ import curses
 
 from log_view import MainWindow, Manager
 from configuration import Configuration
-from sqlite3_driver import Sqlite3Driver
+
+def get_drivers():
+    result = {}
+    try:
+        import sqlite3_driver
+        result['sqlite3'] = sqlite3_driver.Sqlite3Driver.Factory
+    except ImportError:
+        pass
+    try:
+        import mysql_driver
+        result['mysql'] = mysql_driver.MySQLDriver.Factory
+    except ImportError:
+        pass
+    return result
 
 def run_app(window):
     manager = Manager(window)
     conf_file = sys.argv[1] if len(sys.argv) >= 2 else '/etc/logviewer.conf'
-    drivers = { 'sqlite3': Sqlite3Driver.Factory }
-    main_window = MainWindow(manager, Configuration(conf_file, drivers))
+    main_window = MainWindow(manager, Configuration(conf_file, get_drivers()))
     manager.run(main_window)
 
 if __name__ == '__main__':
